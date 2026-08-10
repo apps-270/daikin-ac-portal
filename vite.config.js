219952610dcf1c5490707ea32e37d103333eacc0
apps-270 <aparnasingh7731@gmail.com>
2026-08-10 15:59:49 +0530

    Configure Vite for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,13 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/daikin-ac-portal/',
 })
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+// })
